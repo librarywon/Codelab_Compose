1cd84b2ff3b8380ed5ab18adf77f94487d4ef46d
librarywon <librarywon@naver.com>
2023-11-02 15:30:36 +0900

    [feat]: add image

--- Brithday/app/src/main/java/com/shoppi/happybirthday/MainActivity.kt
+++ Brithday/app/src/main/java/com/shoppi/happybirthday/MainActivity.kt
@@ -3,7 +3,9 @@ package com.shoppi.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,11 +28,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingText("Happy Birthday Jaewon!", "From Harry")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    GreetingImage("Happy Birthday Sam!", "From Emma")
                 }
             }
         }
@@ -62,6 +62,26 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday Seo!", "From Harry")
+        GreetingImage("Happy Birthday Seo!", "From Harry")
+    }
+}
+
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_background)
+    //Step 3 create a box to overlap image and texts
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
