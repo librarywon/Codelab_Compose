5d1d8982ff099bbcf8fd202a7a90edfef8452344
librarywon <librarywon@naver.com>
2023-11-02 17:10:24 +0900

    fourth dimension done

--- ComposeFourDimesion/app/src/main/java/com/librarywon/composefourdimesion/MainActivity.kt
+++ ComposeFourDimesion/app/src/main/java/com/librarywon/composefourdimesion/MainActivity.kt
@@ -3,13 +3,25 @@ package com.librarywon.composefourdimesion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.librarywon.composefourdimesion.ui.theme.ComposeFourDimesionTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +43,70 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        Modifier.fillMaxWidth()
+    ) {
+        Row(Modifier.weight(1f)) {
+            InsideText(
+                title = stringResource(R.string.first_title),
+                text = stringResource(R.string.first_text),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            InsideText(
+                title = stringResource(R.string.second_title),
+                text = stringResource(R.string.second_text),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            InsideText(
+                title = stringResource(R.string.third_title),
+                text = stringResource(R.string.third_text),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            InsideText(
+                title = stringResource(R.string.fourth_title),
+                text = stringResource(R.string.fourth_text),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun InsideText(
+    title: String,
+    text: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = text,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
